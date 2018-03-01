e024ec523a243c31acc2178cdf1bf528ff4259a3
Van <lly219@gmail.com>
2018-03-01 11:47:58 +0800

    :copyright: index

--- src/main/webapp/sym/sym.js
+++ src/main/webapp/sym/sym.js
@@ -1,24 +1,27 @@
 (function () {
-  // first screen
-  $('.first-screen').height($(window).height());
 
-  $('.first-screen .panel').height($(window).height() - 66);
+  if($('.first-screen').length === 1) {
+    // first screen
+    $('.first-screen').height($(window).height());
 
-  // scroll
-  $(window).scroll(function () {
-    if ($('body').height() < $(window).height() * 2 + $('.header').outerHeight()) {
-      return;
-    }
-    if ($(window).scrollTop() > $(window).height()) {
-      $('.header').addClass('header-fixed');
-      $('.first-screen .required').css('padding-top', '116px')
-      $('.header-a').removeClass('current header-a-first');
-    } else {
-      $('.header').removeClass('header-fixed');
-      $('.first-screen .required').css('padding-top', '50px')
-      $('.header-a').addClass('current header-a-first');
-    }
-  });
+    $('.first-screen .panel').height($(window).height() - 66);
+
+    // scroll
+    $(window).scroll(function () {
+      if ($('body').height() < $(window).height() * 2 + $('.header').outerHeight()) {
+        return;
+      }
+      if ($(window).scrollTop() > $(window).height()) {
+        $('.header').addClass('header-fixed');
+        $('.first-screen .required').css('padding-top', '116px')
+        $('.header-a').removeClass('current header-a-first');
+      } else {
+        $('.header').removeClass('header-fixed');
+        $('.first-screen .required').css('padding-top', '50px')
+        $('.header-a').addClass('current header-a-first');
+      }
+    });
+  }
 
   // baidu
   var hm = document.createElement("script");
