c9e9ca361278da290835df3f2458fa65e3c12237
Van <lly219@gmail.com>
2018-11-22 22:17:08 +0800

    :memo: add timeline

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -24,72 +24,75 @@
 var Index = {
   getNews: function () {
     $.ajax({
-      url: "https://hacpai.com/apis/news",
-      type: "GET",
-      dataType: "jsonp",
-      jsonp: "callback",
+      url: 'https://hacpai.com/apis/news',
+      type: 'GET',
+      dataType: 'jsonp',
+      jsonp: 'callback',
       error: function () {
-        $("#dynamic").html("加载新闻列表失败 :-(").css("background-image", "none");
+        $('#dynamic').html('加载新闻列表失败 :-(').css('background-image', 'none')
       },
       success: function (data, textStatus) {
-        var articles = data.articles;
+        var articles = data.articles
         if (0 === articles.length) {
-          $("#dynamic").html("无新闻").css("background-image", "none");
-          return;
+          $('#dynamic').html('无新闻').css('background-image', 'none')
+          return
         }
 
-        var listHTML = "<ul>";
+        var listHTML = '<ul>'
         for (var i = 0; i < articles.length && i < 9; i++) {
-          var article = articles[i];
-          var articleLiHtml = "<li>"
-            + "<a target='_blank' href='" + article.articlePermalink + "'>"
-            + article.articleTitle + "</a>&nbsp;<span>[" + Index._getDate(article.articleCreateTime)
-            + "]</span></li>";
-          listHTML += articleLiHtml;
+          var article = articles[i]
+          var articleLiHtml = '<li>'
+            + '<a target=\'_blank\' href=\'' + article.articlePermalink + '\'>'
+            + article.articleTitle + '</a>&nbsp;<span>[' +
+            Index._getDate(article.articleCreateTime)
+            + ']</span></li>'
+          listHTML += articleLiHtml
         }
-        listHTML += "</ul>";
+        listHTML += '</ul>'
 
-        $("#dynamic").html(listHTML).css("background-image", "none");
-      }
-    });
+        $('#dynamic').html(listHTML).css('background-image', 'none')
+      },
+    })
   },
   _getDate: function (a) {
-    var c = new Date(a);
+    var c = new Date(a)
     var d = c.getFullYear().toString().substr(2, 2),
       e = c.getMonth() + 1,
-      f = c.getDate();
+      f = c.getDate()
 
     if (e < 10) {
-      e = "0" + e;
+      e = '0' + e
     }
 
     if (f < 10) {
-      f = "0" + f;
+      f = '0' + f
     }
-    return d + "-" + e + "-" + f;
+    return d + '-' + e + '-' + f
   },
   initTimeline: function () {
-    if (/Mobile/i.test(navigator.userAgent) || $("#timeline").length === 0) {
-      return;
-    }
-    $("#timeline").height($(window).height() - 110);
-    var timeline = new VMM.Timeline();
-    timeline.init();
+    $('#timelineRender').height($(window).height() - 66)
+    new TL.Timeline('timelineRender',
+      'js/index-timeline.json', {
+        type: 'timeline',
+        width: '100%',
+        start_at_end: true,
+        language: 'zh-cn',
+      })
   },
   killBrowser: function () {
     if ($.browser.msie && parseInt($.browser.version) < 9) {
-      window.location = "/kill-browser.html";
+      window.location = '/kill-browser.html'
     }
-  }
+  },
 };
 
 (function () {
-  Index.killBrowser();
-  Index.getNews();
-  Index.initTimeline();
+  Index.killBrowser()
+  Index.getNews()
+  Index.initTimeline()
 
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?174a1111f4c1724383232f848510cd2c";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
+  var hm = document.createElement('script')
+  hm.src = '//hm.baidu.com/hm.js?174a1111f4c1724383232f848510cd2c'
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+})()
