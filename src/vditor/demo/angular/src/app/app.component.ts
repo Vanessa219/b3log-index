0e80840589592f6e1398bdd61abba881097bc851
Liyuan Li <v@b3log.org>
2020-05-18 11:42:14 +0800

    :memo: vditor angular

--- src/vditor/demo/angular/src/app/app.component.ts
+++ src/vditor/demo/angular/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Vditor from 'vditor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit{
+  vditor: Vditor;
+
+  ngOnInit(): void {
+    this.vditor = new Vditor('vditor', {
+      toolbarConfig: {
+        pin: true,
+      },
+      cache: {
+        enable: false,
+      },
+      minHeight: 500,
+      after: () => {
+        this.vditor.setValue(`
+\`\`\`typescript
+import {Component} from '@angular/core';
+import Vditor from 'vditor';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'angular';
+  vditor: Vditor;
+
+  ngOnInit(): void {
+    this.vditor = new Vditor('vditor', {
+      toolbarConfig: {
+        pin: true,
+      },
+      cache: {
+        enable: false,
+      },
+      minHeight: 500,
+      after: () => {
+        this.vditor.setValue('Hello, React + Vditor!')
+      }
+    })
+  }
+}
+\`\`\`
+`);
+      }
+    });
+  }
 }
