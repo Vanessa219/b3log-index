57a1681e885cd5fab14f9563f02f2bf04671676f
Liyuan Li <v@b3log.org>
2020-05-18 12:52:46 +0800

    :memo: add vditor baidu

---
+++ src/vditor/vditor.js
@@ -0,0 +1,6 @@
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?3035c273d83b0b76e762b7397c790e84";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
