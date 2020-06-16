d035b84abdef0412e5fc86b631e9316f7e29cebb
Liyuan Li <v@b3log.org>
2020-06-16 10:58:44 +0800

    :art: https://github.com/Vanessa219/vditor/issues/489

--- src/vditor/vditor.js
+++ src/vditor/vditor.js
@@ -1,6 +1,42 @@
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?3035c273d83b0b76e762b7397c790e84";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
+(function () {
+  var hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?3035c273d83b0b76e762b7397c790e84'
+  var s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+
+  if (typeof Vcomment === 'function') {
+    const vcomment = new Vcomment({
+      id: 'vditorComments',
+      postId: '1549638745630',
+      url: 'https://hacpai.com',
+      userName: 'Vanessa',
+      currentPage: 1,
+      vditor: {
+        hljsEnable: true,
+        hljsStyle: 'github',
+      },
+      after () {
+        const commentCntElement = document.getElementById('commentsCount')
+        document.getElementById(
+          'commentCnt').innerText = commentCntElement.innerText + ' 个讨论'
+        commentCntElement.nextSibling.remove()
+        commentCntElement.remove()
+      },
+      error () {
+        document.getElementById('comments').remove()
+      },
+    })
+
+    vcomment.render()
+  }
+
+  const demoCodeElement = document.getElementById('vditorDemoCode')
+  if (demoCodeElement) {
+    if (demoCodeElement.textContent.trim() === '') {
+      demoCodeElement.firstElementChild.firstElementChild.innerHTML = '  ' + document.getElementById(
+        'vditorScript').textContent.trimLeft()
+    }
+    Vditor.highlightRender({lineNumber: true, enable: true}, demoCodeElement)
+    Vditor.codeRender(demoCodeElement)
+  }
+})()
