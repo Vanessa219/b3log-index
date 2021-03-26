371dfc4c9a8c893a7578eed793b39aa12aedaf5f
Vanessa <lly219@gmail.com>
2021-03-26 11:14:59 +0800

    :arrow_up: vditor

--- src/vditor/vditor.js
+++ src/vditor/vditor.js
@@ -79,7 +79,7 @@ const autoType = () => {
   })
 }
 
-addStyle('https://cdn.jsdelivr.net/npm/vditor@3.8.3/dist/index.css')
+addStyle('https://cdn.jsdelivr.net/npm/vditor@3.8.4/dist/index.css')
 document.addEventListener('DOMContentLoaded', function () {
   var hm = document.createElement('script')
   hm.src = 'https://hm.baidu.com/hm.js?174a1111f4c1724383232f848510cd2c'
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', function () {
   autoType();
 
   if (document.getElementById('vditorComments')) {
-    addScript('https://cdn.jsdelivr.net/npm/vditor@3.8.3/dist/index.min.js',
+    addScript('https://cdn.jsdelivr.net/npm/vditor@3.8.4/dist/index.min.js',
       () => {
         const demoCodeElement = document.getElementById('vditorDemoCode')
         if (demoCodeElement) {
