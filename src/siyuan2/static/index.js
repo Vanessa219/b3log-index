a09479a6d945738ef29087d84d57547672b74a16
Liang Ding <d@b3log.org>
2021-06-24 17:42:13 +0800

    :art: 更新思源

--- src/siyuan2/static/index.js
+++ src/siyuan2/static/index.js
@@ -169,15 +169,15 @@ const getOS = () => {
     }
 
     // 块级双链图片切换
-    blockImgElement.src = 'static/feature3-1.png'
+    blockImgElement.src = 'https://cdn.jsdelivr.net/gh/vanessa219/b3log-index@9fe7af122eb36d77a47a47ef07bf5d7933b38074/src/siyuan/static/feature3-1.png'
     if (top >= block1Element.offsetTop + block1Element.offsetParent.offsetTop +
       block1Element.clientHeight - 58 &&
       top < block2Element.offsetTop + block2Element.offsetParent.offsetTop +
       block2Element.clientHeight - 58) {
-      blockImgElement.src = 'static/feature3-2.png'
+      blockImgElement.src = 'https://cdn.jsdelivr.net/gh/vanessa219/b3log-index@9fe7af122eb36d77a47a47ef07bf5d7933b38074/src/siyuan/static/feature3-2.png'
     } else if (top >= block2Element.offsetTop +
       block2Element.offsetParent.offsetTop + block2Element.clientHeight - 58) {
-      blockImgElement.src = 'static/feature3-3.png'
+      blockImgElement.src = 'https://cdn.jsdelivr.net/gh/vanessa219/b3log-index@9fe7af122eb36d77a47a47ef07bf5d7933b38074/src/siyuan/static/feature3-3.png'
     }
   })
 })()
