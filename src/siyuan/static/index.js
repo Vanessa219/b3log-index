f8d6846768bd22737caa0414b861630d70b580b5
Liang Ding <d@b3log.org>
2022-02-26 12:06:48 +0800

    :art: 上线官网英文版 https://github.com/siyuan-note/siyuan/issues/4103

--- src/siyuan/static/index.js
+++ src/siyuan/static/index.js
@@ -1,15 +1,29 @@
 const type = () => {
   const typeElement = document.querySelector('#slogan')
-  const texts = [
-    '随时随地离线可用',
-    '端到端加密保证隐私安全',
-    '优雅的 Markdown 编辑体验',
-    '文字创作时沉浸在内容构思中',
-    '触摸灵感和思绪的清晰脉络',
-    '将知识碎片构建成结构化的体系',
-    '找到那些埋藏在记忆深处的灵犀',
-    '思源有云，往来随心',
-  ]
+  let texts;
+  if ('en' !== lang) {
+    texts = [
+      '随时随地离线可用',
+      '端到端加密保证隐私安全',
+      '优雅的 Markdown 编辑体验',
+      '文字创作时沉浸在内容构思中',
+      '触摸灵感和思绪的清晰脉络',
+      '将知识碎片构建成结构化的体系',
+      '找到那些埋藏在记忆深处的灵犀',
+      '思源有云，往来随心',
+    ]
+  } else {
+    texts = [
+      'Available offline anytime, anywhere',
+      'End-to-end encryption ensures privacy and security',
+      'Elegant Markdown editing experience',
+      'Immerse yourself in content ideas while writing',
+      'Touch a clear vein of inspiration and thought',
+      'Build knowledge fragments into a structured system',
+      'Find those consonants buried deep in memory',
+      'SiYuan has a cloud, and you can communicate with your heart',
+    ]
+  }
   let textLength = 0
   let time = 0
   texts.forEach((text, i) => {
@@ -31,7 +45,7 @@ const type = () => {
         time += 50
         setTimeout(() => {
           typeElement.innerHTML = typeElement.textContent.substr(0,
-            typeElement.textContent.length - 1) +
+              typeElement.textContent.length - 1) +
             '<span class="second__caret"></span>'
         }, time)
       }
@@ -141,43 +155,37 @@ const getOS = () => {
     })
     if (top >= document.getElementById('feature1').offsetTop - 58 && top <
       document.getElementById('feature2').offsetTop - 58) {
-      navigationElement.querySelector('.item[data-id="feature1"]').
-        classList.
-        add('item--select')
+      navigationElement.querySelector('.item[data-id="feature1"]').classList.add('item--select')
     } else if (top >= document.getElementById('feature2').offsetTop - 58 &&
       top <
       document.getElementById('feature3').offsetTop - 58) {
-      navigationElement.querySelector('.item[data-id="feature2"]').
-        classList.
-        add('item--select')
+      navigationElement.querySelector('.item[data-id="feature2"]').classList.add('item--select')
     } else if (top >= document.getElementById('feature3').offsetTop - 58 &&
       top <
       document.getElementById('feature4').offsetTop - 58) {
-      navigationElement.querySelector('.item[data-id="feature3"]').
-        classList.
-        add('item--select')
+      navigationElement.querySelector('.item[data-id="feature3"]').classList.add('item--select')
     } else if (top >= document.getElementById('feature4').offsetTop - 58 &&
       top <
       document.getElementById('feature5').offsetTop - 58) {
-      navigationElement.querySelector('.item[data-id="feature4"]').
-        classList.
-        add('item--select')
+      navigationElement.querySelector('.item[data-id="feature4"]').classList.add('item--select')
     } else if (top >= document.getElementById('feature5').offsetTop - 58) {
-      navigationElement.querySelector('.item[data-id="feature5"]').
-        classList.
-        add('item--select')
+      navigationElement.querySelector('.item[data-id="feature5"]').classList.add('item--select')
     }
 
     // 块级双链图片切换
-    blockImgElement.src = 'static/feature3-1.png'
+    let pathPrefix = ""
+    if ('en' === lang) {
+      pathPrefix = "../"
+    }
+    blockImgElement.src = pathPrefix + 'static/feature3-1.png'
     if (top >= block1Element.offsetTop + block1Element.offsetParent.offsetTop +
       block1Element.clientHeight - 58 &&
       top < block2Element.offsetTop + block2Element.offsetParent.offsetTop +
       block2Element.clientHeight - 58) {
-      blockImgElement.src = 'static/feature3-2.png'
+      blockImgElement.src = pathPrefix + 'static/feature3-2.png'
     } else if (top >= block2Element.offsetTop +
       block2Element.offsetParent.offsetTop + block2Element.clientHeight - 58) {
-      blockImgElement.src = 'static/feature3-3.png'
+      blockImgElement.src = pathPrefix + 'static/feature3-3.png'
     }
   })
 })()
